92b1a69f4980ec62f73fc2e9c09e7675a11871a0
203789971@qq.com <203789971@qq.com>
2020-05-16 17:49:12 +0800

    z

---
+++ js/index.js
@@ -0,0 +1,22 @@
+window.addEventListener('load',function(){
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction : 'vertical',
+        pagination: {
+            el:'.swiper-pagination',
+        },
+        mousewheel: true,
+        on:{
+            init: function(){
+                swiperAnimateCache(this);
+                swiperAnimate(this);
+            },
+            slideChangeTransitionEnd: function(){
+                swiperAnimate(this);
+            }
+        },
+        lazy: {
+            loadPrevNext: true,
+            loadPrevNextAmount: 2,
+        }
+    })
+})
